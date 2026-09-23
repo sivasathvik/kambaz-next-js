c8901fbea1f2d057c6277ce25b4f9abaa4190b23
sivasathvik <65842942+sivasathvik@users.noreply.github.com>
2026-09-23 17:56:12 -0400

    name change tweak

--- app/labs/page.tsx
+++ app/labs/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Labs() {
   return (
     <div id="wd-labs">
-        <h2>Paturi Siva Sathvik</h2>
+        <h2>Siva Sathvik Paturi</h2>
         <Link href= "https://github.com/sivasathvik/kambaz-next-js" id= "wd-github">
           Github
         </Link>
